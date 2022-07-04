73852a98b925c726f0423d07884b3ed22a2bb99b
Aleksandra Świerkowska <aleksandra.swierkowska@tum.de>
2022-07-04 12:07:52 +0200

    add message fields

--- src/message/message.py
+++ src/message/message.py
@@ -1,3 +1,5 @@
 class Message:
     def __init__(self, stream_id, message_type, seq_number):
-        pass
+        self.stream_id = stream_id
+        self.message_type = message_type
+        self.seq_number = seq_number
